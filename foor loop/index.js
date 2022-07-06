1a810a8aad1b0ab41945fa73446603e1b1880d15
Mujahed-8654 <mujahedkhan8654@gmail.com>
2022-07-06 16:11:01 +0530

    mk/deleted

--- foor loop/index.js
+++
@@ -1,8 +0,0 @@
-console.log("for loop js file");
-// for loop
-
-for (let i = 3; i <= 20; i++ ){
-    console.log("3*", i, "=",i * 3);
-}
-    const animal = ['cat','dog','cow','horse',,'rabbit'];
-    // console.log (animal.pop());
